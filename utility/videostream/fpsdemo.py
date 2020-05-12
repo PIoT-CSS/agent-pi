020516589d664891edded60c747f31f3e5533249
nugyn <s3429599@student.rmit.edu.au>
2020-05-12 14:42:07 +1000

    refactor, removed redundant code

--- utility/videostream/fpsdemo.py
+++ utility/videostream/fpsdemo.py
@@ -13,36 +13,6 @@ ap.add_argument("-d", "--display", type=int, default=-1,
 	help="Whether or not frames should be displayed")
 args = vars(ap.parse_args())
 
-# grab a pointer to the video stream and initialize the FPS counter
-# print("[INFO] sampling frames from webcam...")
-stream=cv2.VideoCapture(0)
-fps=FPS().start()
-
-# loop over some frames
-while fps._numFrames < args["num_frames"]:
-
-    # grab the frame from the stream and resize it to have a maximum
-    # width of 400 pixels
-    (grabbed, frame)=stream.read()
-    frame=imutils.resize(frame, width=400)
-
-    # check to see if the frame should be displayed to our screen
-    if args["display"] > 0:
-       cv2.imshow("Frame", frame)
-       key=cv2.waitKey(1) & 0xFF
-
-    # update the FPS counter
-    fps.update()
-
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-# do a bit of cleanup
-stream.release()
-cv2.destroyAllWindows()
-
 # created a *threaded* video stream, allow the camera sensor to warmup,
 # and start the FPS counter
 print("[INFO] sampling THREADED frames from webcam...")
