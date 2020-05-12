954ce689bef682c7e439c7e1d84d0422f609674d
nugyn <linhduyn@gmail.com>
2020-05-12 09:34:29 +0100

    increased FPS of videostreaming

--- utility/videostream/fpsdemo.py
+++ utility/videostream/fpsdemo.py
@@ -4,29 +4,32 @@ from imutils.video import FPS
 import argparse
 import imutils
 import cv2
-
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=100,
-                help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=-1,
-                help="Whether or not frames should be displayed")
-args = vars(ap.parse_args())
+import face_recognition
 
 # created a *threaded* video stream, allow the camera sensor to warmup,
 # and start the FPS counter
 print("[INFO] sampling THREADED frames from webcam...")
 vs = WebcamVideoStream(src=0).start()
 fps = FPS().start()
+
 # loop over some frames...this time using the threaded stream
-while fps._numFrames < args["num_frames"]:
+while True:
     # grab the frame from the threaded video stream and resize it
     # to have a maximum width of 400 pixels
     frame = vs.read()
     frame = imutils.resize(frame, width=400)
+    
+    rgb_frame = frame[:, :, ::-1]
     # check to see if the frame should be displayed to our screen
     cv2.imshow("Frame", frame)
+    
+    # Find all the faces in the current frame of video
+    #face_locations = face_recognition.face_locations(rgb_frame)
 
+    # Display the results
+    #for top, right, bottom, left in face_locations:
+        # Draw a box around the face
+    #    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     # update the FPS counter
